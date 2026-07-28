e5bfc0f244737a15610d5aa8ed8b7cb22b254942
arya12354 <aryasfe@atomicmail.io>
2026-07-29 02:23:05 +0330

    Change Telegram bot token
    Updated the Telegram bot token to a new value.

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = os.getenv("8908985330:AAHCe-LThgu5T6S_03gQZin83x2agEx370w")
+TOKEN = os.getenv("8562268215:AAGt7vOpgX2mPFalhEpqAzQIGGSU1XLpO2c")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ ربات روشن شد")
