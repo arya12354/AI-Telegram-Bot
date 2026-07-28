f9ec21ff812cc43ff9b0f2f51efac0fdc71a7ba9
arya12354 <aryasfe@atomicmail.io>
2026-07-29 02:10:06 +0330

    Implement Telegram bot with start and message handlers

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
 
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = os.getenv("8908985330:AAHCe-LThgu5T6S_03gQZin83x2agEx370w")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("✅ ربات روشن شد")
+
+async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    await update.message.reply_text("پیام تو: " + text)
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT, message))
+
+print("Bot Running...")
+app.run_polling()
